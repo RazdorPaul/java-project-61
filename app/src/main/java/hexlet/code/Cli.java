661b7176d2462ff4c9978c34d05c0d6370cb22de
Раздорожный Павел <kaiser15@DESKTOP-Q8CPORC>
2024-10-25 17:17:08 +0900

    Add Makefile, Cli.java, mod App.java, buildgradle.kts

---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+class Cli {
+    public static void helloUser() {
+        Scanner scan =new Scanner(System.in);
+        System.out.print("May I have your name? ");
+        var nameUser = scan.next();
+        scan.close();
+        System.out.println("Hello, "+nameUser);
+    }
+}
