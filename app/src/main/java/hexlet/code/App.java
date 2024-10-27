18861b5adc2d7568653c6e53bffcf4f985e23b1c
Раздорожный Павел <kaiser15@LAPTOP-LO6MJLTN>
2024-10-27 19:14:40 +0900

    modify App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -4,6 +4,5 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter. ");
         Engine.setGame();
-        System.out.println("Goodbye, program is done!");
     }
 }
