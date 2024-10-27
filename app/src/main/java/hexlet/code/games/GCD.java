df27e4b0c1c6b0d641393a27c0c3ea7c6171d71a
Раздорожный Павел <kaiser15@LAPTOP-LO6MJLTN>
2024-10-27 15:51:31 +0900

    add GCD.java

---
+++ app/src/main/java/hexlet/code/games/GCD.java
@@ -0,0 +1,23 @@
+package hexlet.code.games;
+
+public class GCD {
+    public static String getQuestion() {
+        int maxValue = 100;
+        String quest = Integer.toString((int)(Math.random() * maxValue) + 1) + " ";
+        quest += Integer.toString((int)(Math.random() * maxValue) + 1);
+        return quest;
+    }
+
+    public static String getAnswer(String quest) {
+        int num1 = Integer.parseInt(quest.split(" ")[0]);
+        int num2 = Integer.parseInt(quest.split(" ")[1]);
+        int gcd = Math.min(num1, num2);
+        while(gcd > 0) {
+            if (num1 % gcd == 0 & num2 % gcd == 0) {
+                break;
+            }
+            gcd--;
+        }
+        return Integer.toString(gcd);
+    }
+}
