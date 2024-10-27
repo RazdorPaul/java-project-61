f4c9efe5f72a806508ddb86b71cc07d357f9925b
Раздорожный Павел <kaiser15@LAPTOP-LO6MJLTN>
2024-10-27 13:50:48 +0900

    add Engine.java

---
+++ app/src/main/java/hexlet/code/Engine.java
@@ -0,0 +1,61 @@
+package hexlet.code;
+
+import hexlet.code.games.Even;
+import hexlet.code.Greet;
+
+class Engine {
+    static String username;
+
+    public static void setGame() {
+        int choiceGame;
+        String[][] listGames = {
+                {"1", " - Greet"},
+                {"2", " - Even", "Answer 'yes' if the number is even, otherwise answer 'no'."},
+                {"3", " - Calc", "What is the result of the expression?"},
+                {"0", " - Exit"}
+        };
+        for (int i = 0; i < listGames.length; i++) {
+            System.out.println("\t"+listGames[i][0]+listGames[i][1]);
+        }
+        System.out.print("Your choice: ");
+        choiceGame = Greet.getChoice();
+        if (choiceGame < 0 | choiceGame > listGames.length) {
+            System.out.println("This game does not exist!");
+            return;
+        }
+        if (choiceGame == 0) {
+            return;
+        }
+        username = Greet.greeting();
+        if (choiceGame == 1) {
+            return;
+        }
+        startGame(listGames[choiceGame - 1][2], choiceGame);
+    }
+
+    public static void startGame(String title, int numGame) {
+        System.out.println(title);
+        String quest = "";
+        String correctAnswer = "";
+        String userAnswer = "";
+        for (int i = 0; i < 3; i++) {
+            if (numGame == 2) {
+                quest = Even.Question();
+                correctAnswer = Even.correctAnswer(quest);
+            }
+            if (numGame == 3) {}
+            System.out.println("Question: " + quest);
+            System.out.println("Your answer: ");
+            userAnswer = Greet.userInput();
+            if (!userAnswer.equals(correctAnswer)) {
+                System.out.println("'" + userAnswer + "'"
+                                    + " is wrong answer ;(. Correct answer was "
+                                    + "'" + correctAnswer + "'");
+                System.out.println("Let's try again, " + username + "!");
+                return;
+            }
+            System.out.println("Correct!");
+        }
+        System.out.println("Congratulations, " + username + "!");
+    }
+}
