62cec747431cdeb580b98278fc4b8374deaf315e
Раздорожный Павел <kaiser15@LAPTOP-LO6MJLTN>
2024-10-27 20:48:00 +0900

    repair hexlet-check

--- app/src/main/java/hexlet/code/Engine.java
+++ app/src/main/java/hexlet/code/Engine.java
@@ -41,33 +41,31 @@ class Engine {
 
     public static void startGame(String[] game) {
         final int countGame = 3;
+        String nameGame = game[1].trim().split(" ")[1];
         System.out.println(game[2]);
         String quest = "";
         String correctAnswer = "";
         String userAnswer;
         for (int i = 0; i < countGame; i++) {
-            switch (Integer.parseInt(game[0])) {
-                case 2 -> {
-                    quest = Even.getQuestion();
-                    correctAnswer = Even.getAnswer(quest);
-                }
-                case 3 -> {
-                    quest = Calc.getQuestion();
-                    correctAnswer = Calc.getAnswer(quest);
-                }
-                case 4 -> {
-                    quest = GCD.getQuestion();
-                    correctAnswer = GCD.getAnswer(quest);
-                }
-                case 5 -> {
-                    quest = Progression.getQuestion();
-                    correctAnswer = Progression.getAnswer(quest);
-                }
-                case 6 -> {
-                    quest = Prime.getQuestion();
-                    correctAnswer = Prime.getAnswer(quest);
-                }
-                default -> { }
+            if (nameGame.equals("Even")) {
+                quest = Even.getQuestion();
+                correctAnswer = Even.getAnswer(quest);
+            }
+            if (nameGame.equals("Calc")) {
+                quest = Calc.getQuestion();
+                correctAnswer = Calc.getAnswer(quest);
+            }
+            if (nameGame.equals("GCD")) {
+                quest = GCD.getQuestion();
+                correctAnswer = GCD.getAnswer(quest);
+            }
+            if (nameGame.equals("Progression")) {
+                quest = Progression.getQuestion();
+                correctAnswer = Progression.getAnswer(quest);
+            }
+            if (nameGame.equals("Prime")) {
+                quest = Prime.getQuestion();
+                correctAnswer = Prime.getAnswer(quest);
             }
             System.out.println("Question: " + quest);
             System.out.print("Your answer: ");
